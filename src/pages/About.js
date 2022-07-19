64f9f53d723195b61921a09bac610d27e5d7e83f
Philani Msweli <philani.msweli@derivco.co.za>
2022-07-19 13:37:29 +0200

    added dumm about us and readme file

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,6 +4,13 @@ export default function About() {
     return (
         <div>
             <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce commodo
+          consequat tellus vel suscipit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce commodo
+          consequat tellus vel suscipit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce commodo
+          consequat tellus vel suscipit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce commodo
+          consequat tellus vel suscipit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce commodo
+          consequat tellus vel suscipit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce commodo
+          consequat tellus vel suscipit</p>
         </div>
     )
 }
